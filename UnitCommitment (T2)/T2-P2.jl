87a03a501c1f48b612dfd5961c773e37512324c7
VicenteGoehring <vgoehringb@uc.cl>
2024-05-27 19:20:26 -0400

    escenarios renovables v1

--- UnitCommitment (T2)/T2-P2.jl
+++ UnitCommitment (T2)/T2-P2.jl
@@ -11,19 +11,19 @@ min_k_sun = 10.20
 max_k_sun = 14.02
 
 # 40 eolicas 20 solares
-forecasts = []
-for i in 1:60
-    ist = [] 
-    for i in 1:24
-        l
-        mu =
-        if (==wind)
-            sigma = (max_k_wind-min_k_wind)/(24-1)
+forecasts = zeros(60,24)
+println(forecasts)
+for i in 1:60 
+    for j in 1:24
+        mu = 10     #acá tomar valor del excel
+        if (1==1)   #acá incluir si es solar o eólica
+            sigma = mu*(max_k_wind-min_k_wind)/(24-1)
         else
-            sigma = (max_k_sun-min_k_sun)/(24-1)
+            sigma = mu*(max_k_sun-min_k_sun)/(24-1)
         end
         sim_norm = rand(Normal(mu,sigma))
-        list=hcat(list,sim_norm)
+        forecasts[i,j] = sim_norm
     end
-    forecasts=vcat(forecasts,list)
 end   
+
+println(forecasts)
