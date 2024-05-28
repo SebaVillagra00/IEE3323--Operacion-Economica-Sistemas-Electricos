7f94db75a36b9def754ecc3c3eb32f883ca2c6e2
VicenteGoehring <vgoehringb@uc.cl>
2024-05-27 23:35:26 -0400

    cambio de nombre carpeta

---
+++ UnitCommitment-T2/T2-P2.jl
@@ -0,0 +1,52 @@
+#Importar paquetes
+using Random, Distributions, Plots
+using DataFrames, CSV
+using JuMP , HiGHS      # https://jump.dev/JuMP.jl/stable/manual/models/ 
+using Gurobi
+using XLSX  # Se agrega para leer archivos .xlsx (Excel)
+#Lectura de excel
+archivo = "Case118.xlsx"    # se comienza con el caso mas pequeño
+xf = XLSX.readxlsx(archivo) # leer el archivo excel
+
+# Separar hojas del archivo
+sheet_buses = xf["Buses"]
+sheet_demandas = xf["Demand"]
+sheet_generadores = xf["Generators"]
+sheet_lineas = xf["Lines"]
+sheet_renovables = xf["Renewables"]
+
+# Definir la función de parada
+# Se crea el DataFrame hasta que se reconozca la entrada "END"
+# De esta forma se puede leer tablas con cualquier numero de filas
+#   siempre y cuando terminen con un "END".
+function stop_condition(row)
+    return isempty(row[1]) || row[1] == "END"
+end
+renovables_ref  = DataFrame(XLSX.gettable(sheet_renovables,"A:Y",first_row=2,stop_in_row_function=stop_condition))
+renewables = copy(renovables_ref) 
+println(renewables)
+
+# inicio simulaciones
+iteraciones = 100
+min_k_wind = 14.70
+max_k_wind = 30.92
+min_k_sun = 10.20
+max_k_sun = 14.02
+
+# 40 eolicas 20 solares
+forecasts = zeros(60,24)
+println(forecasts)
+for i in 1:60 
+    for j in 1:24
+        mu = 10     #acá tomar valor del excel
+        if (1==1)   #acá incluir si es solar o eólica
+            sigma = mu*(max_k_wind-min_k_wind)/(24-1)
+        else
+            sigma = mu*(max_k_sun-min_k_sun)/(24-1)
+        end
+        sim_norm = rand(Normal(mu,sigma))*1000
+        forecasts[i,j] = round(sim_norm)/1000
+    end
+end   
+
+println(forecasts)
